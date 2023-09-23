8f84ad4edd075b2864469f815428d05a1a5f891d
Andrew <21520144@gm.uit.edu.vn>
2023-09-23 12:23:53 -0500

    Add navigate bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './views/Nav.js';
 
 function App() {
+  let name = "Andrew Nguyen";
+  let obj = {name:"Eric", year:"2023"};
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world with React.js (Hoi Dan IT)
+          Hello world with React.js and {name} {JSON.stringify(obj)};
         </p>
         <a
           className="App-link"
