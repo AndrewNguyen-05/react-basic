ed7c876971f7a99c186d3abec5f2ba50f2630c60
Andrew Nguyen <21520144@gm.uit.edu.vn>
2023-10-05 13:08:40 +0700

    Add files and changes

--- src/views/Countdown.js
+++ src/views/Countdown.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 
-class ClassCountDown extends React.Component{
+class ClassCountDown extends React.Component {
     state = {
         count: 10
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.Timer = setInterval(() => {
             this.setState({
                 count: this.state.count - 1
@@ -14,8 +14,8 @@ class ClassCountDown extends React.Component{
         }, 1000);
     }
 
-    componentDidUpdate(prevProps, prevState){
-        if(prevState.Timer !== this.state.count && this.state.count === 0){
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.Timer !== this.state.count && this.state.count === 0) {
             clearInterval(this.Timer);
             // this.props.alertOnTime();
         }
@@ -30,26 +30,22 @@ class ClassCountDown extends React.Component{
 
 const HookCountDown = (props) => {
     const [count, setCount] = useState(10);
-
     useEffect(() => {
-        if(count === 0){
+        if (count === 0) {
             props.alertOnTime();
             return;
         }
-
         let timer = setInterval(() => {
             setCount(count - 1);
-        },  1000)
+        }, 1000);
 
         return () => {
-            clearInterval(timer)
+            clearInterval(timer);
         }
-    
     }, [count])
-
     return (
         <div>{count} / Hooks</div>
     )
 }
 
-export {ClassCountDown, HookCountDown};
+export { ClassCountDown, HookCountDown };
