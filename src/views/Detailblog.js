58c34d5c1bfb8c769b5b67c93ccc7178d357c948
Andrew Nguyen <21520144@gm.uit.edu.vn>
2023-10-08 20:27:28 +0700

    Back to blog from detailblog

--- src/views/Detailblog.js
+++ src/views/Detailblog.js
@@ -1,9 +1,17 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom";
+import { useParams, useHistory } from "react-router-dom/cjs/react-router-dom";
 
 const Detailblog = () => {
     let { id } = useParams();
+    let history = useHistory();
+    const handleBackData = () => {
+        history.push('/blog');
+    }
     return (
-        <div>Hello detail blog with id = {id}</div>
+        <div>
+            <div onClick={handleBackData}>&lt;--Back</div>
+            <div>Hello detail blog with id = {id}</div>
+        </div>
+
     )
 }
 
